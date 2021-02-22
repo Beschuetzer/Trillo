4731d32b9c061c285227f0510dd3870653644f79
Adam <adam.j.major@gmail.com>
2021-02-22 15:58:08 -0800

    Finish user menu functionality

--- JS/main.js
+++ JS/main.js
@@ -1,6 +1,21 @@
 const userIcon = document.querySelector('.user-nav__user');
 userIcon.addEventListener('click', userIconHandler);
 
+const userNavDropdownItems = document.querySelectorAll('.user-nav__dropdown-item');
+for (let i = 0; i < userNavDropdownItems.length; i++) {
+    const item = userNavDropdownItems[i];
+    item.addEventListener('mouseenter', dropDownEnterHandler);
+}
+
 function userIconHandler(e) {
     e.currentTarget?.children[e.currentTarget.children?.length -1].classList.toggle('hidden');
+}
+
+function dropDownEnterHandler(e) {
+    for(i = 0; i < userNavDropdownItems.length; i++) {
+        const child = userNavDropdownItems[i]?.children[0];
+        child.classList?.remove('hidden');
+        child.style.right = '25%';
+        // e.currentTarget.children[0].classList.remove('hidden');
+    }
 }
