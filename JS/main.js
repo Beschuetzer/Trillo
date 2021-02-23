9127bb8938e0f30124e1ffe9f3f6115bdfb52c67
Adam <adam.j.major@gmail.com>
2021-02-23 09:22:55 -0800

    Dropdown for user done

--- JS/main.js
+++ JS/main.js
@@ -1,8 +1,8 @@
 const userIcon = document.querySelector('.user-nav__user');
-userIcon.addEventListener('click', userIconHandler);
+userIcon.addEventListener('click', navHandler);
 
 const msgIcon = document.querySelector('#messages');
-msgIcon.addEventListener('click', messagesHandler);
+msgIcon.addEventListener('click', navHandler);
 
 const userNavDropdownItems = document.querySelectorAll('.user-nav__dropdown-item');
 for (let i = 0; i < userNavDropdownItems.length; i++) {
@@ -16,15 +16,18 @@ links.forEach(link => link.addEventListener('click', linkClickHandler));
 function linkClickHandler(e) {
     const linkName = e.currentTarget?.children[0]?.textContent;
     if (linkName?.match(/search/i)) document.querySelector('.search__input').focus();
-    else if (linkName?.match(/message/i)) messagesHandler({currentTarget: msgIcon});
+    else if (linkName?.match(/message/i)) navHandler({currentTarget: msgIcon});
 }
 
 
-function userIconHandler(e) {
+function navHandler(e) {
+    if (e.currentTarget.id.match(/messages/i)) userIcon?.children[userIcon.children?.length -1].classList.add('hidden');
+    else if (e.currentTarget.className.match(/user-nav__user/i)) msgIcon?.children[msgIcon.children?.length - 1].classList.add('hidden');
     e.currentTarget?.children[e.currentTarget.children?.length -1].classList.toggle('hidden');
 }
 
 function dropDownEnterHandler(e) {
+
     document.querySelector('.user-nav__dropdown').style.width = '100%';
     for(i = 0; i < userNavDropdownItems.length; i++) {
         const child = userNavDropdownItems[i]?.children[0];
@@ -34,7 +37,7 @@ function dropDownEnterHandler(e) {
     }
 }
 
-function messagesHandler(e) {
-    debugger;
-    
-}
+// function navHandler(e) {
+//     e.currentTarget?.children[e.currentTarget.children?.length -1].classList.toggle('hidden');
+//     debugger;
+// }
